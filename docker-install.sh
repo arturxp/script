632cd4b6f41784a7a21bfbded623926115e2b6c0
arturxp <48387397+arturxp@users.noreply.github.com>
2024-09-17 16:20:19 +0300

    Add files via upload

---
+++ docker-install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+user=$(logname)
+sudo apt-get update && \
+sudo apt-get upgrade -y
+
+sudo apt-get install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings 
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+sudo groupadd docker
+sudo usermod -aG docker $user
+
+newgrp docker
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+
+sudo reboot
