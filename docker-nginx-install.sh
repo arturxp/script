fe0b1a0deb89c14d4b762bb5569f5134bb5a1f0a
arturxp <48387397+arturxp@users.noreply.github.com>
2024-09-19 10:13:36 +0300

    Update docker-nginx-install.sh

--- docker-nginx-install.sh
+++ docker-nginx-install.sh
@@ -12,11 +12,11 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update -y
+sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
-##########################
+
 
 sudo apt install curl gnupg2 ca-certificates lsb-release ubuntu-keyring -y
 
@@ -27,7 +27,7 @@ echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
 http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
     | sudo tee /etc/apt/sources.list.d/nginx.list
 
-echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" \
-    | sudo tee /etc/apt/preferences.d/99nginx
+echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n"
+sudo tee /etc/apt/preferences.d/99nginx
 
 sudo apt update
