7080d330fd98fb73a92a0d93d208231ac4a3366e
arturxp <48387397+arturxp@users.noreply.github.com>
2024-09-19 15:51:13 +0300

    Update docker-nginx-install.sh

--- docker-nginx-install.sh
+++ docker-nginx-install.sh
@@ -18,7 +18,7 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 
 ##########
 
-sudo apt install curl gnupg2 ca-certificates lsb-release ubuntu-keyring -y
+sudo apt-get install curl gnupg2 ca-certificates lsb-release ubuntu-keyring -y
 
 curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
     | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
@@ -26,11 +26,11 @@ curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
 gpg --dry-run --quiet --no-keyring --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
 
 echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
-http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
-    | sudo tee /etc/apt/sources.list.d/nginx.list
+http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" | \
+sudo tee /etc/apt/sources.list.d/nginx.list
 
-echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" \
-    | sudo tee /etc/apt/preferences.d/99nginx
+echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" | \
+sudo tee /etc/apt/preferences.d/99nginx
     
-sudo apt update
-#sudo apt install nginx -y
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install nginx -y
