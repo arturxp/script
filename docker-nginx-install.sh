e404b478d0da277aef63588e4e2450dd9f7fb739
arturxp <48387397+arturxp@users.noreply.github.com>
2024-09-19 15:05:45 +0300

    Update docker-nginx-install.sh

--- docker-nginx-install.sh
+++ docker-nginx-install.sh
@@ -16,7 +16,7 @@ sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
-
+##########
 
 sudo apt install curl gnupg2 ca-certificates lsb-release ubuntu-keyring -y
 
