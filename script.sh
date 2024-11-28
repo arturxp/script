bed41ace969b741540b4c5e7d1d7256462f5e21b
Artur Kirkoriyan <artur@outlook.com>
2024-11-28 19:45:22 +0200

    first commit

---
+++ script.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+user=$(logname)
+ apt-get update && \
+ apt-get upgrade -y
+
+ apt-get install ca-certificates curl -y
+ install -m 0755 -d /etc/apt/keyrings 
+ curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+ chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+   tee /etc/apt/sources.list.d/docker.list > /dev/null
+ apt-get update
+
+ apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+##########
+
+ apt-get install curl gnupg2 ca-certificates lsb-release ubuntu-keyring -y
+
+curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
+    |  tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
+
+gpg --dry-run --quiet --no-keyring --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
+http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" | \
+ tee /etc/apt/sources.list.d/nginx.list
+
+echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" | \
+ tee /etc/apt/preferences.d/99nginx
+    
+ apt-get update &&  apt-get upgrade -y
+ apt-get install nginx -y
+
+##########
+
+ snap install --classic certbot
+
+ ln -s /snap/bin/certbot /usr/bin/certbot
+
+telinit 6
