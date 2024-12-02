9db3b779cf8cc3d9e856882bb102a55bf39b0557
Artur Kirkoriyan <artur@outlook.com>
2024-12-02 14:47:30 +0200

    Refactor docker-nginx-install.sh for improved formatting and consistency

--- docker-nginx-install.sh
+++ docker-nginx-install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 user=$(logname)
-sudo apt-get update && \
-sudo apt-get upgrade -y
+sudo apt-get update &&
+  sudo apt-get upgrade -y
 
 sudo apt-get install ca-certificates curl -y
-sudo install -m 0755 -d /etc/apt/keyrings 
+sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+  sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
@@ -20,18 +20,18 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 
 sudo apt-get install curl gnupg2 ca-certificates lsb-release ubuntu-keyring -y
 
-curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
-    | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
+curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor |
+  sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
 
 gpg --dry-run --quiet --no-keyring --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
 
 echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
-http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" | \
-sudo tee /etc/apt/sources.list.d/nginx.list
+http://nginx.org/packages/ubuntu $(lsb_release -cs) nginx" |
+  sudo tee /etc/apt/sources.list.d/nginx.list
 
-echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" | \
-sudo tee /etc/apt/preferences.d/99nginx
-    
+echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" |
+  sudo tee /etc/apt/preferences.d/99nginx
+
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install nginx -y
 
